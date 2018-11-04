959ae98368cfd57e37733b0d1668680e202afc5f
Andrew80Ka <andrew80ka@gmail.com>
2018-11-04 14:28:54 +0300

    second commit
    add script slider

---
+++ docs/js/slideshow.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+	var slideIndex = 0;
+
+	showPhotos();
+
+	function showPhotos(){
+
+		var slides = $('.photo img');
+		console.log(slides);
+		console.log(slides.length);
+
+		// Запускаем цикл для скрытия фотографий
+		for (var i = 0; i < slides.length; i++) {
+			console.log(i);
+			$(slides[i]).hide();
+		}
+
+		slideIndex++;
+
+		console.log(slideIndex);
+
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+
+		$(slides[slideIndex-1]).show();
+
+		setTimeout(showPhotos, 1000);
+
+	}
+});
