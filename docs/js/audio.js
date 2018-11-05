98f59e73335c6bf15345d40dff34f3a0001aabd9
Andrew80Ka <andrew80ka@gmail.com>
2018-11-05 12:35:36 +0300

    add audio player

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+	var audio = $('#audioTrack')[0];
+	console.log(audio);
+
+	$playBtn = $('#audio-play');
+	$pauseBtn = $('#audio-pause');
+	$audioControl = $('[data-type="audio-control"]');
+
+	// initAudioPlayer();
+	setTimeout(initAudioPlayer, 2000);
+	
+	function initAudioPlayer() {
+		// $playBtn.parent().show();
+		$pauseBtn.parent().show();
+		audio.loop = true;
+		audio.play();
+
+		$audioControl.click(function(){
+			console.log('audio control click');
+
+			if (audio.paused) {
+				audio.play();
+				$playBtn.parent().hide();
+				$pauseBtn.parent().show();
+				console.log('playing now');
+			} else {
+				audio.pause();
+				$pauseBtn.parent().hide();
+				$playBtn.parent().show();
+				console.log('pause now');
+			}
+
+		});
+	}
 
 
 
+	// audio.play();
 });
